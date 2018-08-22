d11d895c9e2528ffd0ce5135f68fdce274838503
Nijinsha <mail.nijinsha@gmail.com>
2018-08-22 20:05:33 +0530

    Adds basic implementation of video list view

--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework import generics
+from rest_framework import generics, permissions
+from .serializers import YoutubeVideoSerializer
+
 # Create your views here.
 
 
@@ -13,3 +15,10 @@ def slack_config_view(request):
     """
     # TODO: get the post data and populate the database with video links.
 
+
+class YoutubeVideoListView(generics.ListAPIView):
+    """
+    List view class to list all the youtube links and its votes.
+    """
+    permission_classes = permissions.AllowAny
+    serializer_class = YoutubeVideoSerializer
