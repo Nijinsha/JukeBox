2b412c637d0cc7e1b1cb06d3cff30e9de956cfa5
Nijinsha <mail.nijinsha@gmail.com>
2018-08-22 19:55:27 +0530

    Adds basic implementation of slack configuration view

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework import generics
 # Create your views here.
+
+
+@api_view(['POST', ])
+def slack_config_view(request):
+    """
+    Function to get the slack configurations.
+    :param request: Request Object.
+    :return:
+    """
+    # TODO: get the post data and populate the database with video links.
+
