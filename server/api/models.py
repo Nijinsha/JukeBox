8625c25d6429e984a80d7b5292b6b181cbed9fab
Nijinsha <mail.nijinsha@gmail.com>
2018-08-22 19:49:40 +0530

    refactor code

--- server/api/models.py
+++ server/api/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class YoutubeVideo(models.Model):
-    """Database to store youtube url and the vote count.
+    """
+    Database to store youtube url and the vote count.
 
     @member_variables
     link: <string> The link to the youtube video.
