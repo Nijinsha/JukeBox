e3c2b7b71c1dc291d7a27c4e5c5a7dac865a8b90
Nijinsha <mail.nijinsha@gmail.com>
2018-08-22 19:03:23 +0530

    adds youtube video model

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class YoutubeVideo(models.Model):
+    """Database to store youtube url and the vote count.
+
+    @member_variables
+    link: <string> The link to the youtube video.
+    vote: <int> The number of vot it go.
+    """
+    link = models.CharField(max_length=12)
+    vote = models.IntegerField(default=0)
