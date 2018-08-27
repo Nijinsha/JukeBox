81bfbd2aaae68ee2f1bd81ba004de766e34d9661
Nijinsha <mail.nijinsha@gmail.com>
2018-08-27 11:31:23 +0530

    Adds url for config available view

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import YoutubeVideoListView, slack_config_view, vote
+from .views import YoutubeVideoListView, slack_config_view, vote, is_config_available_view
 
 urlpatterns = [
     path('slack_config/', slack_config_view, name="slack config"),
     path('videos/', YoutubeVideoListView.as_view(), name="videos"),
     path('vote/<uid>/', vote, name="vote"),
+    path('config-available/', is_config_available_view, name="is available"),
 ]
