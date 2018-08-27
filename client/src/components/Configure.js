b039e53ce3a236ff617709acef2dd4e55f70c77a
Nijinsha <mail.nijinsha@gmail.com>
2018-08-27 12:23:54 +0530

    Adds Configure component

---
+++ client/src/components/Configure.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Configure extends Component {
+    render() {
+        return (
+          <h1>Configrure Page</h1>
+        );
+    }
+}
+
+export default Configure;
