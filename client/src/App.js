6b5b260c15a4812b52838097305b8ab7d9643f53
Nijinsha <mail.nijinsha@gmail.com>
2018-08-27 11:42:42 +0530

    Add axios api call to check if config available and constructor

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      configurationAvailable: false
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:8000/api/v1/config-available/`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ configurationAvailable: res.data.available });
+        this.setState({ isLoading: false });
+      });
+
+  }
+
   render() {
     return (
       <div className="App">
-        
+
       </div>
     );
   }
