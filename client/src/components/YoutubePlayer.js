2acacc6d9327c71e8f8504f5e40cd4af0fa77b15
Nijinsha <mail.nijinsha@gmail.com>
2018-08-27 12:34:08 +0530

    Adds YoutubePlayer component

---
+++ client/src/components/YoutubePlayer.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+let videoList=[];
+let i=0;
+class YoutubePlayer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.videoId=null;
+    }
+    sortByVote(){
+        videoList.sort(function(a, b) {
+            return parseInt(b.vote) - parseInt(a.vote);
+        });
+    }
+    componentDidMount() {
+        console.log(this.props.videoList)
+        videoList=this.props.videoList
+        this.sortByVote();
+        console.log("after sort");
+        console.log(videoList[i]);
+        this.setState({ videoId: videoList[i].video });
+    }
+
+    render() {
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                autoplay: 1
+            }
+        };
+
+        return (
+            <YouTube
+                videoId={this.state.videoId}
+                opts={opts}
+                onReady={this._onReady}
+                onEnd={this._onEnd}
+            />
+        );
+    }
+
+    _onEnd = (event) => {
+        if(i===videoList.length-1){
+            i=-1;
+        }
+        this.setState({ videoId: videoList[++i].video });
+        event.target.playVideo();
+    }
+
+}
+export default YoutubePlayer;
